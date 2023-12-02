744b164aa373cf030b05b5e943f9b7af6de2012d
aoijudith <152441671+aoijudith@users.noreply.github.com>
2023-12-02 12:52:15 -0800

    Create RandomWalk2d.py

---
+++ RandomWalk2d.py
@@ -0,0 +1,23 @@
+import random as r
+import numpy as np
+import pylab
+import math as m
+import matplotlib as mpl
+
+N=1000
+
+x=np.zeros(N)
+y=np.zeros(N)
+
+for i in range(1,N):
+    dx=r.randint(-1,1)
+    dy=r.randint(-1,1)
+    L=m.sqrt(dx**2+dy**2)
+    while L>0:
+        x[i]=x[i-1]+dx/L
+        y[i]=y[i-1]+dy/L
+
+
+mpl.rcParams['lines.linewidth'] = 0.5
+pylab.plot(x, y, markersize=1,marker=".")
+pylab.show()
