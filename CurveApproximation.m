c7082b996c50b4a581cd5548418819c5fdb6a4b1
aoijudith <152441671+aoijudith@users.noreply.github.com>
2023-12-03 09:46:36 -0800

    Update CurveApproximation.m

--- CurveApproximation.m
+++ CurveApproximation.m
@@ -5,14 +5,14 @@ close all
 
 h=.125
 t=0.0:h:1.0
-y=zeros(size(t));
-exact=zeros(size(t));
-error=zeros(size(t));
+y=zeros(size(t));       %Euler's method approximation
+exact=zeros(size(t));   %Exact function
+error=zeros(size(t));   %Error of Euler's method approximation
 y(1)=0.0;
 exact(1)=0.0;
 for n=1:8
-    y(n+1)=y(n)+h*cos(y(n))
-    exact(n+1)=asin((exp(2*n*h)-1)/(exp(2*n*h)+1))
+    y(n+1)=y(n)+h*cos(y(n)) 
+    exact(n+1)=asin((exp(2*n*h)-1)/(exp(2*n*h)+1)) 
     error(n+1)=abs(y(n)-exact(n));
 end
 plot(t,y,t,exact,'--');
